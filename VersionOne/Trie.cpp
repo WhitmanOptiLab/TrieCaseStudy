a9195f4e6b173f973ce43ffcf1b0f4cc247aa5f0
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-10-14 10:25:01 -0700

    Create Trie.cpp

---
+++ VersionOne/Trie.cpp
@@ -0,0 +1,33 @@
+
+#include "Trie.h"
+#include "Node.h"
+
+Trie::Trie(){
+    //Set the root node as a new node
+    root = getNode();
+}
+
+void Trie::insert(string key){
+    Node *pCrawl = root;
+    for(int i = 0;i < key.length();i++){
+        int index = key[i] - 'a';
+        if(!pCrawl->children[index])
+            pCrawl->children[index] = new Node();
+        pCrawl = pCrawl->children[index];
+    }
+    pCrawl->isEndOfWord = true;
+}
+bool Trie::search(string key){
+    Node* pCrawl = root;
+    for(int i = 0;i < key.length();i++){
+        int index = key[i] - 'a';
+        if(!pCrawl->children[index])
+            return false;
+        pCrawl = pCrawl->children[index];
+    }
+    return (pCrawl->isEndOfWord);
+}
+Node* Trie::getNode(){
+    Node* temp = new Node();
+    return temp;
+}
