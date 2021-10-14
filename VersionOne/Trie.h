06ea661ffa576751e784de145f8782c2a427c845
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-10-14 10:24:49 -0700

    Create Trie.h

---
+++ VersionOne/Trie.h
@@ -0,0 +1,24 @@
+#ifndef TRIE_H
+#define TRIE_H
+//#define MAX_CHAR 100
+//typedef char String[MAX_CHAR+1];
+#include <iostream>
+#include <fstream> 
+#include <vector>
+#include <ctime>
+#include <unistd.h>
+#include <chrono>
+#include "Node.h"
+
+
+class Trie{
+    public:
+        Trie();
+        void insert(string key);
+        bool search(string key);
+    private:
+        Node* root;
+        Node* getNode();
+};
+
+#endif
