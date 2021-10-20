3d0d3db590aae25d032c74391a61e395e0947ca0
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-10-20 15:10:12 -0700

    Update TrieNode.h

--- Version2/TrieNode.h
+++ Version2/TrieNode.h
@@ -25,6 +25,7 @@ class Trie{
         Trie();
         void insert(string key);
         bool search(string key);
+        Node* getRoot();
     private:
         Node* root;
         Node* getNode();
