7a5792ba12abfb3a8c1e3dcc46529d5921cbfbd8
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-10-26 20:30:22 -0700

    Update Node.h

--- VersionOne/Node.h
+++ VersionOne/Node.h
@@ -7,12 +7,13 @@ using namespace std;
 
 class Node{
     public:
-        bool EndOfWord();
+       
         Node();
         Node *children[26];//Alphabet size could be smaller/bigger than 26 characters
-        bool isEndOfWord;
+        bool isEndOfWord();
+        void setEndOfWord();
     private:
-        
+        bool EndOfWord;
 };
 
 
