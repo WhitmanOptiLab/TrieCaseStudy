470ac6c1c4b036bd68aea69695d56547c72125c1
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-10-27 15:30:00 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -63,23 +63,6 @@ string genSequence(int length){
     cout << ret << endl;
     return ret;
 }
-//This seems quite slow, why not just make a hash table if we are doing it this way?
-void SearchForWords(struct Node* root,string s,int i,bool visited[],string newStr){
-    visited[i] = true;
-    newStr += s[i];
-    if((newStr.length() >= 3) && search(root,newStr) == true){
-        cout << newStr << endl;
-    }
-    for(int i = 0;i < s.length();i++){
-        if(visited[i] == false){
-            SearchForWords(root,s,i,visited,newStr);
-        }
-    }
-    newStr = newStr.substr(0,newStr.length()-1);
-    //newStr = "" + newStr[newStr.length()-1];
-    visited[i] = false;
-}
-
 //Further Research Needed: https://en.wikipedia.org/wiki/Aho–Corasick_algorithm
 //This function works, recursively goes throught the trie
 void subsetSearch(struct Node* root,string s,bool visited[],string newStr){
