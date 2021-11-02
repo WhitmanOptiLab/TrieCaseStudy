250ae4352f728f35084c3b48bb4179b62260ed08
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-11-01 17:18:00 -0700

    Update Trie.cpp

--- TrieClass/Trie.cpp
+++ TrieClass/Trie.cpp
@@ -3,29 +3,34 @@
 #include "Node.h"
 
 Trie::Trie(){
-    
+    //Set the root node as a new node
+    root = getNode();
 }
 
-void Trie::insert(Node* root,string key){
+void Trie::insert(string key){
     Node *pCrawl = root;
     for(int i = 0;i < key.length();i++){
         int index = key[i] - 'a';
-        if(!pCrawl->children[index])
-            pCrawl->children[index] = new Node();
-        pCrawl = pCrawl->children[index];
+        if(!pCrawl->getChild(index))
+            pCrawl->setChild(index);
+        pCrawl = pCrawl->getChild(index);
     }
+    pCrawl->setEndOfWord();
 }
-bool Trie::search(Node* root,string key){
+bool Trie::search(string key){
     Node* pCrawl = root;
     for(int i = 0;i < key.length();i++){
         int index = key[i] - 'a';
-        if(!pCrawl->children[index])
+        if(!pCrawl->getChild(index))
             return false;
-        pCrawl = pCrawl->children[index];
+        pCrawl = pCrawl->getChild(index);
     }
-    return (pCrawl->isEndOfWord);
+    return (pCrawl->isEndOfWord());
 }
 Node* Trie::getNode(){
     Node* temp = new Node();
     return temp;
+}
+Node* Trie::getRoot(){
+    return root;
 }
