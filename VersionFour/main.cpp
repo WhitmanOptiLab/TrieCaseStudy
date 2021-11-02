e0a23005099375eb80685872a7f04733ce4f2e5b
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-11-01 17:27:47 -0700

    Update main.cpp

--- VersionFour/main.cpp
+++ VersionFour/main.cpp
@@ -14,6 +14,23 @@ using namespace std;
 
 vector<string> Dict;
 
+void subSetSearch(Trie* t, bool visited[],int i,string str,string seq){
+    visited[i] = true;
+    str += seq[i];
+    //If its in the hashtable print it out
+    if(t->search(str) && str.length() > 3){
+        cout << str << endl;
+    }
+    for(int id = 0;id < seq.length();id++){
+        if(visited[id] == false){
+            subSetSearch(t,visited,id,str,seq);
+        }
+    }
+    visited[i] = false;
+    str = str.substr(0,str.length()-1);
+}
+
+
 int main(){
     //reading file and pushing content to vector Dict
     ifstream file;
@@ -33,13 +50,33 @@ int main(){
     }
 
     
-    auto t1 = std::chrono::high_resolution_clock::now();
+    //auto t1 = std::chrono::high_resolution_clock::now();
     string testSeq = "testwordao";
 
-    t->subsetSearch(testSeq);
+    /*
+    for(int i = 0;i < testSeq.length();i++){
+        bool visited[testSeq.length()];
+        string newStr = "";
+        //newStr += testWord[i];
+        //int index = testWord[i] - 'a';
+        //visited[i] = true;
+        subSetSearch(t,visited,i,newStr,testSeq);
+    }
+    */
+      //run this for the tire node subset class
+   
+    auto t1 = std::chrono::high_resolution_clock::now();
+    t->subsetSearch("testwordao");
+    auto t2 = std::chrono::high_resolution_clock::now();
+    cout << "MilliSeconds: " << std::chrono::duration_cast<std::chrono::milliseconds>(t2-t1).count() << endl;
+    
+    
     
 
-    auto t2 = std::chrono::high_resolution_clock::now();
-    cout << "MicroSeconds: " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << endl;
+    //auto t2 = std::chrono::high_resolution_clock::now();
+    //cout << "MicroSeconds: " << std::chrono::duration_cast<std::chrono::microseconds>(t2-t1).count() << endl;
+    
+    cout << "bobobob" << endl;
+
     
 }
