1d1f2577e15588699a0107d5fedde79d44d7648a
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-11-01 17:17:37 -0700

    Update Node.cpp

--- TrieClass/Node.cpp
+++ TrieClass/Node.cpp
@@ -3,8 +3,20 @@
 #include "Node.h"
 
 Node::Node(){
-    isEndOfWord = false;
+    EndOfWord = false;
     for(int i = 0;i < 26;i++){
         children[i] = NULL;
     }
+}
+bool Node::isEndOfWord(){
+    return EndOfWord;
+}
+void Node::setEndOfWord(){
+    EndOfWord = true;
+}
+Node* Node::getChild(int index){
+    return children[index];
+}
+void Node::setChild(int index){
+    children[index] = new Node();
 }
