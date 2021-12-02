01d998d75f7985b2dc36b75dd5a011d3d628712c
Colin Aslett <35709150+ColinAslett@users.noreply.github.com>
2021-12-01 20:15:43 -0800

    Update TrieNodeSubset.h

--- VersionFour/TrieNodeSubset.h
+++ VersionFour/TrieNodeSubset.h
@@ -14,10 +14,12 @@ using namespace std;
 class Node{
     public:
         Node();
-        Node *children[26];//Alphabet size could be smaller/bigger than 26 characters
         bool isEndOfWord();
         void setEndOfWord();
+        Node* getChild(int index);
+        void setChild(int index);
     private:
+        Node *children[26];//Alphabet size could be smaller/bigger than 26 characters
         bool EndOfWord;
 };
 class Trie{
